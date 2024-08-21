c846b7a2ae11fba3af8bf009cddb5824c542c752
— Chichiii? <cintarahmaliyasyifa04@gmail.com>
2024-08-21 14:50:17 +0700

    first
    hehe

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const backgroundContainer = document.querySelector('.background-container');
+
+});document.querySelector('.get-container').addEventListener('click', function() {
+  window.location.href = 'oke.html';
+});
