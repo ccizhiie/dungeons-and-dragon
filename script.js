c480065e883b2951b61f030f72ee90e1aa312af9
— Chichiii? <cintarahmaliyasyifa04@gmail.com>
2024-09-01 23:20:45 +0700

    test

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const backgroundContainer = document.querySelector('.background-container');
-
-});document.querySelector('.get-container').addEventListener('click', function() {
-  window.location.href = 'oke.html';
-});
